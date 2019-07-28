efbd6caece50e2261bb121841534f0f0b40d0391
Vladimir Mikhalenko <mihalenko.w.a@gmail.com>
2019-07-28 13:30:14 +0300

    shopping-list animation

--- src/app/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list/shopping-list.component.ts
@@ -8,11 +8,84 @@ import { Store } from '@ngrx/store';
 import { State } from '../store/shopping-list.reducer';
 import * as ShoppingListActions from '../store/shopping-list.actions';
 import * as fromRoot from '../../store/app.reducer';
+import { animate, group, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
+  animations: [
+    trigger('ingredientsListItem',
+      [
+        state('in',
+          style(
+            {
+              opacity: 1,
+              transform: 'translateX(0)'
+            }
+          )
+        ),
+        transition('void => *',
+          [
+            animate(300,
+              keyframes([
+                style(
+                  {
+                    transform: 'translateX(-100px)',
+                    opacity: 0,
+                    offset: 0 // percent of animate timings
+                  }
+                ),
+                style(
+                  {
+                    transform: 'translateX(-50px)',
+                    opacity: 0.5,
+                    offset: 0.4
+                  }
+                ),
+                style(
+                  {
+                    transform: 'translate(-20px)',
+                    opacity: 1,
+                    offset: 0.8
+                  }
+                ),
+                style(
+                  {
+                    transform: 'translate(0)',
+                    opacity: 1,
+                    offset: 1
+                  }
+                )
+              ])
+            )
+          ]
+        ),
+        transition('* => void',
+          [
+            // group executed in parallel
+            group([
+              animate(300,
+                style(
+                  {
+                    color: 'red'
+                  }
+                )
+              ),
+              animate(300,
+                style(
+                  {
+                    opacity: 0,
+                    transform: 'translate(100px)'
+                  }
+                )
+              )
+            ])
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class ShoppingListComponent extends BaseComponent implements OnInit {
 
@@ -30,7 +103,7 @@ export class ShoppingListComponent extends BaseComponent implements OnInit {
     this.ingredients$ = this.store.select('shoppingList');
     this.ingredients$
       .pipe(
-        map(state => state.editedIngredientId),
+        map(storeState => storeState.editedIngredientId),
         takeUntil(this.alive$)
       )
       .subscribe(editedIngredientId => {
